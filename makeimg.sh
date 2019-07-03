a1e62b9d7744728e73ca14aeb134c932abfdb52d
tdmadam <52431348+tdmadam@users.noreply.github.com>
2019-07-02 20:04:18 -0400

    Update makeimg.sh

--- makeimg.sh
+++ makeimg.sh
@@ -54,22 +54,22 @@ function firmware_patch_img {
 ## dd if=$FIRMWARE_OUT bs=1 skip=124 count=32 | hexdump -C  --> Can be used to display content of 32 bytes before change
 
 dd if=/dev/zero of=$FIRMWARE_OUT bs=1 seek=124 count=32 conv=notrunc
-##  write 32 bytes of zeros(/dev/zero) to a file starting at byte 124 and don't truncate the output
+##  write 32 bytes of zeros(/dev/zero) to a file starting at the offset 124 and don't truncate the output
 
     echo -n $SWVER | dd of=$FIRMWARE_OUT bs=1 seek=124 conv=notrunc
-## write $SWVER to the previously zeroed out area starting at byte 124
+## write $SWVER to the previously zeroed out area starting at the offset 124
 
     dd if=/dev/zero of=$FIRMWARE_OUT bs=1 seek=632 count=32 conv=notrunc
-##  write 32 bytes of zeros(/dev/zero) to a file starting at byte 632 and don't truncate the output
+##  write 32 bytes of zeros(/dev/zero) to a file starting at the offset 632 and don't truncate the output
 
     echo -n $SWVER | dd of=$FIRMWARE_OUT bs=1 seek=632 conv=notrunc
-## write $SWVER to the previously zeroed out area starting at byte 124
+## write $SWVER to the previously zeroed out area starting at the offset 124
 
     dd if=/dev/zero of=$FIRMWARE_OUT bs=1 seek=664 count=32 conv=notrunc
-##  write 32 bytes of zeros(/dev/zero) to a file starting at byte 664 and don't truncate the output
+##  write 32 bytes of zeros(/dev/zero) to a file starting at the offset 664 and don't truncate the output
 
     echo -n $VENDOR | dd of=$FIRMWARE_OUT bs=1 seek=664 conv=notrunc
-## write $VENDOR to the previously zeroed out area starting at byte 664
+## write $VENDOR to the previously zeroed out area starting at the offset 664
 
 ## --------------------- Update created time ------------------------------------------------------------------------
 # binwalk displays created time for squashfs at 0x140200. Update created time later at the image offset 0x208
@@ -90,7 +90,7 @@ dd if=/dev/zero of=$FIRMWARE_OUT bs=1 seek=124 count=32 conv=notrunc
 
     htime=$(byte_hex2bin $htime 1)
 ## reverse and convert 596F891B to binary format with \x prefix
-## Just for info check existing time at 0x208 --> ab 26 6f 59 byte reverse=596f26ab dec=1500456619
+## Just for info check existing time at the offset 0x208 --> ab 26 6f 59 byte reverse=596f26ab dec=1500456619
 ## dd if=$FIRMWARE_IN bs=1 skip=520 count=4 | hexdump -C  
        
     echo -e "${htime}" | dd of=$FIRMWARE_OUT bs=1 seek=520 count=4 conv=notrunc
@@ -112,7 +112,7 @@ function byte_hex2bin {
 ## --------------------- Calculate CRC -----------------------------------------------------------------------
 function firmware_crc {
     dd if=/dev/zero of=$FIRMWARE_OUT bs=1 seek=$1 count=4 conv=notrunc
-## zero out 4 bytes where CRC32 is stored 0x21C 0x220 0x68 (540 544 104)
+## zero out 4 bytes where CRC32 is stored the offset 0x21C 0x220 0x68 (540 544 104)
 
     crc=`$CRC32 $FIRMWARE_OUT $3 $4 | tail -n 1 | awk '{print $2}' | sed "s/0x//1"`
 ## calculate CRC32 $offset $length (get the last line, 2nd colum and remove 1st instance of 0x)
